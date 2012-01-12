c4974502358fdddc1759db35c32b35d390a4cd05
James Brotchie <brotchie@gmail.com>
2012-01-12 12:02:59 +1000

    Added distutils setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='excelbt',
+      version='1.0',
+      description='Excel Build Toolkit',
+      author='James Brotchie',
+      author_email='brotchie@gmail.com',
+      url='https://bitbucket.org/brotchie/excelbt',
+      packages=['excelbt'],
+     )
